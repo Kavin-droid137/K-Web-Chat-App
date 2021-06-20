868bfd732ed4b3586bf1e4f0fc4f080542753003
Kavin Ganesh R <77374022+Kavin-droid137@users.noreply.github.com>
2021-06-20 16:35:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+  return (
+  <ChatEngine 
+    height="100vh" 
+    projectID="6c781777-ee1a-43ed-8507-4a77ffc0240b" 
+    userName="Kavin777Ganesh"
+    userSecret="asdf"
+    />
+    )
+};
+export default App;
